df657bc66087e429af283a0e43a4ad8dc5491679
Karen Vizcaino <karenvizcaino0316@gmail.com>
2026-04-06 21:04:50 +0000

    Cambios SQL

--- cleaner.sql
+++ cleaner.sql
@@ -927,3 +927,31 @@ INSERT INTO pago VALUES (28,'PayPal','ak-std-000022','2009-01-13',8489);
 INSERT INTO pago VALUES (30,'PayPal','ak-std-000024','2009-01-16',7863);
 INSERT INTO pago VALUES (35,'PayPal','ak-std-000025','2007-10-06',3321);
 INSERT INTO pago VALUES (38,'PayPal','ak-std-000026','2006-05-26',1171);
+
+/*SENTENCIAS DML PARA PRACTICAR SIN MORIR EN EL INTENTO*/
+/*EJEMPLO PARA CONOCER LAS COLUMNAS  DE UNA TABLA/*
+DESCRIBE EMPLEADO;
+SELECT codigo_emmpleado,nombre,apellido1.apellido2,
+extensio,email,codigo_oficina,,codigo_jefe
+puesto FROM EMPLEADO;
+/*RETO 1 - Retorna  un listado con el codigo de oficina y la ciudad donde hay oficinas/*
+describe oficinas;
+select o.codigo_oficina as cod_oficina,o.ciudad country;
+concat (o.codigo_oficina,'-',o.ciudad) as cod_ciudad_oficina
+from oficina o;
+/*RETO 2 - Retorna  un listado con la ciudad y el telefono de las oficinas en españa/*
+
+SELECT ciudad,telefono,pais from oficina
+where upper (pais) = 'ESPAÑA';
+
+/*RETO 3 - Retorna el listado con todos los clientes que sean de la ciudad de madrid cuyo representante de ventas tenga el codigo de empleado 11 o 30. /*
+ 
+ describe clientes;
+ describe empleado;
+ select cl.ciudad,em.codigo_empleado from cliente cl
+ join empleado em
+ on em.codigo_empleado = cl.codigo_empleado_rep_ventas
+ where upper (cl.ciudad) = 'MADRID'
+ and (em.codigo_empleado = 11
+ or em.codigo_empleado = 30);
+ 
